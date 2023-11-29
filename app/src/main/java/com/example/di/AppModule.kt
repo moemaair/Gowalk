605bf28c242fd7ea6dd6fa7af24d5a9984bdb333
moemaair <rageali12@gmail.com>
2023-11-29 19:02:17 +0300

    add Appmodule object for dependency injection

---
+++ app/src/main/java/com/example/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.example.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
